1ad54859d4b2d0dd8a5e218b20ad26ba0f9e46a9
Vic <vicxd@pm.me>
2026-05-20 16:02:58 +0200

    basic bot that embeds a site in a modal

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Proof of Concept of e2ee communication inside the slack client using the video embed block and RSA public-private key encryption.
 
+
+The idea is that the bot embeds a website that generates a simple webapp which, embedded in the slack app, allow you to:
+- create a rsa public and private key which is stored in the server. The private key is encrypted with a passphrase so the service can't decrypt it.
+- Once the user has a private key, allow them to encrypt messages targeting other registered users RSA public keys.
+
+The de-encrypted message never leaves the users embed. All the encryption is done locally.
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,128 @@
 import Slack, {} from "@slack/bolt";
-const { App, subtype } = Slack;
-
+const { App, subtype, ExpressReceiver } = Slack;
+import {Eta} from "eta"
 import { config } from "dotenv";
+import { randomUUID, UUID } from "crypto"
+import path from "path";
 config();
+
+
 
 const {
 	SLACK_BOT_TOKEN,
 	SLACK_SIGNING_SECRET,
 	SLACK_APP_TOKEN,
 	SLACK_CLIENT_SECRET,
+  SELF_BASE_URL,
+  PORT
 } = process.env;
+
+//This is so silly but tscompiler is trash
+const eta = new Eta({views: path.join(import.meta.dirname,"../src", "templates")})
+
+const receiver = new ExpressReceiver({signingSecret: SLACK_SIGNING_SECRET!,});
+
+interface PageKind {
+  user: string; // slack id 
+  kind: 'registration'; // kind of page 
+}
+
+const slugs = new Map<string, PageKind>()
+
+//DEBUG
+slugs.set("quecosa", {
+  kind: "registration",
+  user: "U1234567"
+})
+
 
 const slack = new App({
 	token: SLACK_BOT_TOKEN,
-	appToken: SLACK_APP_TOKEN,
-	socketMode: true, 
-	signingSecret: SLACK_SIGNING_SECRET,
-	port: 6777,
-	clientSecret: SLACK_CLIENT_SECRET,
+  installerOptions: { port: 3000 },
+	receiver,
 });
 
+
+
+
+interface User {
+  slack_id: string;
+  pub_key: string;
+  private_key: string; // Private keys should ALWAYS have a passphrase
+}
+
+
+
 slack.command(
-	"/command",
+	"/e2ee",
 	async ({ ack, body, client, respond, command }) => {
 		await ack();
 		const args = body.text;
+
+
+
+    const slug = generateSlug({user: body.user_id, kind: "registration"})
+    const targetVideoUrl = `${SELF_BASE_URL}/slug/${slug}`
+
+    const responseBlocks = [
+        {
+          type: "video",
+          alt_text: "embedded e2ee client",
+          title: {
+            type: "plain_text",
+            text: "E2EE Slack",
+          },
+        thumbnail_url: "https://http.cat/200", // TODO change this with an actual thumbnail 
+        video_url: targetVideoUrl
+        },
+      ]
+      slack.logger.info("Sending user to targetVideoUrl =",targetVideoUrl)
+
+    //await respond({
+    //  response_type: "ephemeral",
+    //  text: "To use e2ee slack you must use a different client",
+    //  blocks: responseBlocks
+    //})
+
+    await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "E2EE Slack"
+        },
+        callback_id: slug,
+        blocks: responseBlocks,
+        submit: {
+          type: "plain_text",
+          text: "done"
+        }
+      }
+    })
 	},
 );
 
-await slack.start();
 
+receiver.router.get("/slug/:slug", (req, res) => {
+  const {slug} = req.params;
+
+  const page = slugs.get(slug)
+
+  if (!page) return res.status(404).send("Slug not found, weird")
+
+  res.status(200).send(eta.render("./something", {name:"que", slug}))
+})
+
+receiver.router.get("/que", (r, res) =>{
+  res.status(200).send("so")
+})
+
+
+function generateSlug(k: PageKind) {
+  const slug = randomUUID();
+  slugs.set(slug, k);
+  return slug;
+}
+await slack.start(PORT!);
+await slack.logger.info("Slack app started in", PORT!)
